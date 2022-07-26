bc826c10e23fd7d60a082ed4e4bdfd57555792d1
David Leuliette <dleuliette@gmail.com>
2022-07-26 10:26:54 +0200

    add eslint-tailwind

--- libs/weshipit-ui/src/lib/header/header.tsx
+++ libs/weshipit-ui/src/lib/header/header.tsx
@@ -1,14 +1,16 @@
+import Link from 'next/link';
+
 export function Header() {
   return (
     <div className="relative bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6">
+        <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <Link href="/">
               <h2 className="text-2xl font-bold">
                 weshipit<span className="text-gray-400">.today</span>
               </h2>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- libs/weshipit-ui/src/lib/hero/hero.tsx
+++ libs/weshipit-ui/src/lib/hero/hero.tsx
@@ -1,12 +1,16 @@
 export function Hero() {
   return (
-    <main className="lg:relative h-full bg-gray-100">
-      <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-72 lg:text-left">
+    <main className="h-full bg-gray-100 lg:relative">
+      <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-center lg:py-72 lg:text-left">
         <div className="px-4  sm:px-8 xl:pr-16">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+          <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
             <span className="block xl:inline">
-              Building React Native apps<br />
-              in <span className=" line-through text-gray-400">months</span> days.
+              Building React Native apps
+              <br />
+              in <span className=" text-gray-400 line-through">
+                months
+              </span>{' '}
+              days.
             </span>
           </h1>
           <p className="mt-3 max-w-md text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
@@ -18,7 +22,8 @@ export function Hero() {
               <a
                 href="https://airtable.com/shrkRxhdc2zJD8EM5"
                 target={'_blank'}
-                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+                rel="noreferrer"
               >
                 Start a project
               </a>
