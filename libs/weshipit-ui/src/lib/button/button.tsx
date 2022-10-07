551311f029b6eb78e1748cc7f619e97351552c26
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-10-07 11:59:40 +0200

    remove duplicate content

--- libs/weshipit-ui/src/lib/button/button.tsx
+++ libs/weshipit-ui/src/lib/button/button.tsx
@@ -15,24 +15,17 @@ export function Button({
   style,
   href,
 }: ButtonProps) {
+  let variantStyle;
   if (variant === 'secondary') {
-    return (
-      <a
-        href={href}
-        target={'_blank'}
-        className={`flex items-center justify-between rounded-md bg-white py-3 text-base  font-medium text-black shadow-md hover:bg-gray-100 md:px-6 md:text-lg ${style}`}
-        rel="noreferrer"
-      >
-        {accessoryLeft && <div className="flex">{accessoryLeft}</div>}
-        {children}
-      </a>
-    );
+    variantStyle = 'bg-white text-black hover:bg-gray-100';
+  } else {
+    variantStyle = 'bg-indigo-600 hover:bg-indigo-700 text-white';
   }
   return (
     <a
       href={href}
       target={'_blank'}
-      className={`flex items-center justify-between rounded-md border border-transparent bg-indigo-600 py-3 text-base font-medium text-white shadow-md hover:bg-indigo-700  md:px-6 md:text-lg ${style}`}
+      className={`flex items-center justify-between rounded-md  py-3 text-base  font-medium  shadow-md  md:px-6 md:text-lg ${style} ${variantStyle}`}
       rel="noreferrer"
     >
       {accessoryLeft && <div className="flex">{accessoryLeft}</div>}
