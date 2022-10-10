ab341e1a601215f36b217bf07fa76a9b3aacad3d
David Leuliette <dleuliette@gmail.com>
2022-10-10 10:35:33 +0200

    Run setup command

---
+++ prismicios.js
@@ -0,0 +1,43 @@
+import * as prismic from '@prismicio/client';
+import * as prismicH from '@prismicio/helpers';
+import * as prismicNext from '@prismicio/next';
+import sm from './sm.json';
+
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint);
+
+// Update the routes array to match your project's route structure
+/** @type {prismic.ClientConfig['routes']} **/
+const routes = [
+  {
+    type: 'homepage',
+    path: '/',
+  },
+  {
+    type: 'page',
+    path: '/:uid',
+  },
+];
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
+ */
+export const createClient = (config = {}) => {
+  const client = prismic.createClient(sm.apiEndpoint, {
+    routes,
+    ...config,
+  });
+
+  prismicNext.enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req,
+  });
+
+  return client;
+};
