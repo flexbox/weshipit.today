23d88db2c4a679d2a9c4014f229cf3c187670ef2
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-10-12 15:19:24 +0200

    update layout style

--- apps/weshipit/components/Layout.tsx
+++ apps/weshipit/components/Layout.tsx
@@ -9,9 +9,7 @@ export function Layout({ children, withHeader }: LayoutProps) {
   return (
     <div className="h-screen bg-gray-100 lg:relative">
       {withHeader && <Header />}
-      <div className="mx-auto w-full px-32 pb-8 text-center lg:py-32 lg:text-left">
-        {children}
-      </div>
+      <div className="mx-auto w-full px-32 ">{children}</div>
     </div>
   );
 }
