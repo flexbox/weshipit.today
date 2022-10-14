d7721099800b211095711643f8c42d7b19e9689f
David Leuliette <dleuliette@gmail.com>
2022-10-14 10:58:36 +0200

    feat: 🎸 better dx with git-cz
    BREAKING CHANGE: 🧨 use git-cz all the time please

---
+++ changelog.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  list: ['feat', 'fix', 'test', 'refactor', 'chore', 'docs'],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: ['type', 'subject', 'breaking', 'issues'],
+};
