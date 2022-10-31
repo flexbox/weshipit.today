40f5bc71d5acd7a424d75ffcdd1698bcd4b399c2
David Leuliette <dleuliette@gmail.com>
2022-10-31 10:41:14 +0100

    test: 💍 add jest-environment-jsdom

--- libs/ui/src/lib/text/text.spec.tsx
+++ libs/ui/src/lib/text/text.spec.tsx
@@ -4,7 +4,7 @@ import Text from './text';
 
 describe('Text', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Text />);
+    const { baseElement } = render(<Text variant="h1">i am a title</Text>);
     expect(baseElement).toBeTruthy();
   });
 });
