779b9e4c2fad7cd4372a163e21bfabafa21dbfe0
David Leuliette <dleuliette@gmail.com>
2022-10-31 10:52:44 +0100

    fix: 🐛 storybook Header
    ✅ Closes: #27

--- libs/ui/src/lib/header/header.tsx
+++ libs/ui/src/lib/header/header.tsx
@@ -6,8 +6,10 @@ export function Header() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <Link href="/" className="text-2xl font-bold">
-              weshipit<span className="text-gray-400">.today</span>
+            <Link href="/">
+              <div className="text-2xl font-bold">
+                weshipit<span className="text-gray-400">.today</span>
+              </div>
             </Link>
           </div>
         </div>
