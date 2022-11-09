2adbc7902114bc343d7f27809b34d14e0a8bd57a
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-11-09 12:08:04 +0100

    fix: 🐛 import api-list in ui/index

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/hero/hero';
 export * from './lib/header/header';
 export * from './lib/clients-list/clients-list';
 export * from './lib/clients-list/clients-list-homepage';
+export * from './lib/api-list/api-list';
