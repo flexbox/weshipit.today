a1f6171f90006bd44dbb848f00b0ec1422043bcf
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-11-09 11:32:38 +0100

    feat: 🎸 add fixture and interface for api-list data

---
+++ libs/ui/src/lib/api-list/api-list.fixture.ts
@@ -0,0 +1,167 @@
+export const clientsListFixture = [
+  {
+    name: 'Amplify',
+    website: 'https://aws.amazon.com/amplify/',
+    description: 'https://docs.amplify.aws/start/q/integration/react-native/',
+    type: 'Infrastructure',
+    features: '',
+    pricing: 'Free tiers,Pay as-you-go',
+  },
+  {
+    name: 'Firebase',
+    website: 'https://firebase.google.com/',
+    description:
+      'Multiple features in one product\n\nhttps://console.firebase.google.com/',
+    type: 'Infrastructure',
+    features:
+      'Hosting,Crash reporting,Remote config,Cloud messaging,Real-time database,Analytics',
+    pricing: 'Free tiers,Pay as-you-go',
+  },
+  {
+    name: 'App Center',
+    website: 'https://appcenter.ms/',
+    description: '',
+    type: 'Infrastructure',
+    features: 'Analytics,Device farm,Crash reporting',
+    pricing: '',
+  },
+  {
+    name: 'Expo',
+    website: 'https://expo.dev/',
+    description:
+      'An open-source platform for making universal native apps with React. Expo runs on Android, iOS, and the web.',
+    type: 'Infrastructure',
+    features: '',
+    pricing: 'Open source',
+  },
+  {
+    name: 'Supabase',
+    website: 'https://supabase.com/',
+    description: 'The Open SourceFirebase Alternative',
+    type: 'Infrastructure',
+    features: 'Auth - Web,Real-time database',
+    pricing: 'Open source',
+  },
+  {
+    name: 'React Native mmkv',
+    website: 'https://github.com/mrousavy/react-native-mmkv',
+    description:
+      'The fastest key/value storage for React Native. ~30x faster than AsyncStorage!',
+    type: 'Database',
+    features: '',
+    pricing: 'Open source',
+  },
+  {
+    name: 'Watermelon DB',
+    website: 'https://nozbe.github.io/WatermelonDB/index.html',
+    description:
+      'Build powerful React and React Native apps that scale from hundreds to tens of thousands of records and remain fast',
+    type: 'Database',
+    features: '',
+    pricing: 'Open source',
+  },
+  {
+    name: 'realm',
+    website: 'https://realm.io/',
+    description:
+      'The Realm React Native SDK allows you to use Realm Database and backend Apps from React Native\n\nTypeScript Template\nhttps://www.npmjs.com/package/@realm/expo-template-ts',
+    type: 'Database',
+    features: '',
+    pricing: 'Open source',
+  },
+  {
+    name: 'PouchDB',
+    website: 'https://pouchdb.com/',
+    description:
+      'PouchDB is an open-source JavaScript database that is based on Apache CouchDB and is optimized for use in browsers. PouchDB was intended to assist web developers in creating apps that are as functional offline as they are online. It allows apps to save data locally while offline, then synchronize it with CouchDB and other compatible servers once the app is back online, ensuring that the user’s data is always up to date.',
+    type: 'Database',
+    features: '',
+    pricing: 'Open source',
+  },
+  {
+    name: 'BugSnag',
+    website: 'https://www.bugsnag.com/',
+    description:
+      'Bugsnag monitors application stability so you can make data-driven decisions on whether you should be building new features, or fixing bugs.',
+    type: 'Bug reports',
+    features: 'Crash reporting',
+    pricing: 'Free tiers,Premium tiers',
+  },
+  {
+    name: 'Sentry',
+    website: 'https://sentry.io',
+    description: 'Application Monitoring and Error Tracking Software',
+    type: 'Bug reports',
+    features: 'Crash reporting',
+    pricing: 'Free tiers,Premium tiers',
+  },
+  {
+    name: 'LogRocket',
+    website: 'https://logrocket.com/',
+    description:
+      'LogRocket combines session replay, performance monitoring, and product analytics – empowering software teams to create the ideal web and mobile product experience',
+    type: 'Bug reports',
+    features: 'Crash reporting',
+    pricing: 'Free tiers,Premium tiers',
+  },
+  {
+    name: 'Stream',
+    website: 'https://getstream.io/',
+    description:
+      'That’s because most developers just use Stream. It’s the #1 chat-messaging and activity feed platform in the world – because it handles all of the mess that comes with building chat apps for you.\n\n\nhttps://getstream.io/chat/trial/',
+    type: 'Backend',
+    features: 'Push notifications,Video,Image & file upload',
+    pricing: '',
+  },
+  {
+    name: 'BaseQL',
+    website: 'https://baseql.com/',
+    description:
+      'BaseQL brings the power of GraphQL to Airtable, allowing you to query, filter and mutate records using a simple-yet-powerful Explorer. Integrate it easily with low-code and no-code applications!',
+    type: 'Backend',
+    features: '',
+    pricing: 'Free tiers,Premium tiers',
+  },
+  {
+    name: 'FusionAuth',
+    website: 'https://fusionauth.io/',
+    description: '',
+    type: 'Authentication',
+    features: '',
+    pricing: 'Free tiers,Premium tiers',
+  },
+  {
+    name: 'Magic Link',
+    website: 'https://magic.link/',
+    description: '',
+    type: 'Authentication',
+    features: 'Email,SMS,Auth - Social Login ,Auth - Web,Auth - Multi-factor',
+    pricing: 'Free tiers,Pay as-you-go',
+  },
+  {
+    name: 'UXCam',
+    website: 'https://uxcam.com/',
+    description: '',
+    type: 'Analytics',
+    features: 'Analytics',
+    pricing: 'Free tiers,Premium tiers',
+  },
+  {
+    name: 'Segment',
+    website: 'https://segment.com/',
+    description:
+      'Segment is a cross-platform data collection and analytics software that supports APIs. It also supports React Native.',
+    type: 'Analytics',
+    features: '',
+    pricing: '',
+  },
+  {
+    name: 'Mixpanel',
+    website: '',
+    description:
+      'Mixpanel allows you to segment and creates funnels to analyze user actions. It also displays data regarding data points such as location, device, channels, and more.',
+    type: 'Analytics',
+    features: 'Push notifications,A/B testing',
+    pricing: '',
+  },
+];
---
+++ libs/ui/src/lib/api-list/clients-list.d.ts
@@ -0,0 +1,12 @@
+export interface apiProps {
+  name: string;
+  website: string;
+  description: string;
+  type: string;
+  features: string;
+  pricing: string;
+}
+
+export interface apiListProps {
+  clients: apiProps[];
+}
