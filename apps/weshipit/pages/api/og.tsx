4a682f37d7c7e1838d8950c4c5668b76c5c82542
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-11-18 15:14:21 +0100

    fix: 🐛 console.log to error

--- apps/weshipit/pages/api/og.tsx
+++ apps/weshipit/pages/api/og.tsx
@@ -69,7 +69,7 @@ export default function handler(req: NextRequest) {
       }
     );
   } catch (e: any) {
-    console.log(`${e.message}`);
+    console.error(`${e.message}`);
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
