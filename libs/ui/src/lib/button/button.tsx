30f64bb46b47e3cc3abc10394bbeb71e0998b025
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-11-23 17:32:22 +0100

    feat: 🎸 add cva to button but doesnt work for the moment

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -1,35 +1,71 @@
-export interface ButtonProps {
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const button = cva('badge', {
+  variants: {
+    intent: {
+      primary: [
+        'bg-indigo-600',
+        'hover:bg-indigo-700',
+        'text-white',
+        'md:text-lg',
+        'md:px-6',
+        'shadow-md',
+        'font-medium',
+        'text-base',
+        'py-3',
+        'rounded-md',
+        'justify-between',
+        'items-center',
+        'flex',
+      ],
+      indigo: [
+        'bg-white',
+        'text-black',
+        'hover:bg-gray-100',
+        'flex',
+        'items-center',
+        'justify-between',
+        'rounded-md',
+        'py-3',
+        'md:text-lg',
+        'md:px-6',
+        'shadow-md',
+        'font-medium',
+        'text-base',
+      ],
+    },
+  },
+  compoundVariants: [{ intent: 'primary' }],
+  defaultVariants: {
+    intent: 'primary',
+  },
+});
+
+export interface ButtonProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof button> {
   variant?: 'primary' | 'secondary';
   accessoryLeft?: React.ReactNode;
   children?: React.ReactNode;
-  style?: string;
-  href: string;
+  // style?: string;
+  // href: string;
 }
 
-export function Button({
-  variant,
-  accessoryLeft,
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  intent,
   children,
-  style,
-  href,
-}: ButtonProps) {
-  let variantStyle;
-  if (variant === 'secondary') {
-    variantStyle = 'bg-white text-black hover:bg-gray-100';
-  } else {
-    variantStyle = 'bg-indigo-600 hover:bg-indigo-700 text-white';
-  }
-  return (
-    <a
-      href={href}
-      target="_blank"
-      className={`flex items-center justify-between rounded-md py-3 text-base font-medium shadow-md md:px-6 md:text-lg ${style} ${variantStyle}`}
-      rel="noreferrer"
-    >
-      {accessoryLeft && <div className="flex">{accessoryLeft}</div>}
-      {children}
-    </a>
-  );
-}
+  accessoryLeft,
+  ...props
+}) => (
+  <a
+    href="google.com"
+    target="_blank"
+    className={button({ intent, className })}
+  >
+    {accessoryLeft && <div className="flex">{accessoryLeft}</div>}
+    {children}
+  </a>
+);
 
 export default Button;
