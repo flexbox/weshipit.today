d21a6d26d9b253434991abaecd964fa8607d400d
David Leuliette <dleuliette@gmail.com>
2022-12-15 17:55:46 +0100

    feat: 🎸 add external link to clearbit

--- libs/ui/src/lib/hyperlink/hyperlink.tsx
+++ libs/ui/src/lib/hyperlink/hyperlink.tsx
@@ -1,24 +1,29 @@
-import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid';
 
 export interface HyperlinkProps {
   href: string;
-  text: string;
+  children: string;
   isExternal?: boolean;
+  className?: string;
 }
 
-export function Hyperlink(props: HyperlinkProps) {
-  const { text } = props;
-  const isExternal = props.isExternal || false;
-
+export function Hyperlink({
+  children,
+  isExternal = false,
+  ...rest
+}: HyperlinkProps) {
   if (isExternal) {
     return (
-      <a target="_blank" rel="noopener noreferrer" {...props}>
-        {text} <ArrowUpRightIcon />
+      <a target="_blank" rel="noopener noreferrer" {...rest}>
+        <span className="flex items-center">
+          {children}
+          <ArrowTopRightOnSquareIcon className="ml-1 h-4 w-4 text-gray-400" />
+        </span>
       </a>
     );
   }
 
-  return <a {...props}>{text}</a>;
+  return <a {...rest}>{children}</a>;
 }
 
 export default Hyperlink;
