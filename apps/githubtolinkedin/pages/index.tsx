18f370da83a6fd6ec8eea6e7f8109532f404a256
David Leuliette <dleuliette@gmail.com>
2023-03-09 14:38:22 +0100

    feat: 🎸 Add new next.js app Github to LinkedIn

---
+++ apps/githubtolinkedin/pages/index.tsx
@@ -0,0 +1,21 @@
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div id="welcome">
+          <h1>
+            <span> Hello there, </span>
+            Welcome githubtolinkedin 👋
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Index;
