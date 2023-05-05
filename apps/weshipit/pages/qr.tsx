cf30f257dc03fc5eeebf2aa96f6b5388a1d58d13
David Leuliette <dleuliette@gmail.com>
2023-05-05 16:07:00 +0200

    feat: 🎸 add @tailwindcss/typography for prose component

--- apps/weshipit/pages/qr.tsx
+++ apps/weshipit/pages/qr.tsx
@@ -2,10 +2,10 @@ import { Text } from '@weshipit/ui';
 import { Layout } from '../components/layout';
 import Script from 'next/script';
 
-/* eslint-disable-next-line */
-export interface QrProps {}
+const PAIR_PROGRAMMING_FORM_EMBED =
+  'https://airtable.com/embed/shrbgrlfblUkIFovF';
 
-export function Qr(props: QrProps) {
+export function Qr() {
   return (
     <Layout
       seoTitle="FREE Pair Programming Session"
@@ -14,39 +14,34 @@ export function Qr(props: QrProps) {
       withFooter
     >
       <div className="mx-auto max-w-2xl p-3">
-        <Text variant="h1">Office Hours</Text>
-        <Text variant="p1">
-          Join us during Code and Coffee breaks to pair with one of our
-          experienced software consultants on a challenge you’re puzzling over!
-          Pick the day and time slot that’s best for you, and tell us what you’d
-          like to pair on.
-        </Text>
-        <Text variant="p1">
-          You’ll receive a copy of your time slot request, and we’ll confirm as
-          we match you to a Double Agent Slots will be filled in order received,
-          and when slots fill up we’ll start a wait list If something comes up,
-          please email hello@testdouble.com so we can give your slot to someone
-          else
-        </Text>
-        <Text variant="p1">
-          If no slots become available you can arrange a virtual pairing session
-          post-conference If the Coffee & Code breaks are not optimal times,
-          email hello@testdouble.com to request an alternate time or a
-          post-conference virtual pairing session.
-        </Text>
-        <Text variant="p1">
-          LinkedIn Profile/Resume Reviews, Blog writing & Marketing are
-          available for pairing with our branding expert throughout the
-          conference.
-        </Text>
-        <Script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></Script>
-        <iframe
-          src="https://airtable.com/embed/shrbgrlfblUkIFovF?backgroundColor=blue"
-          width="100%"
-          height="1231"
-          className="airtable-embed airtable-dynamic-height mb-12"
-          style={{ background: '#ffffff' }}
-        ></iframe>
+        <div className="prose prose-lg">
+          <h1 className="mt-8">Office Hours</h1>
+          <p>
+            Join us during Code and Coffee breaks to pair with one of our
+            experienced software consultants on a challenge you’re puzzling
+            over!{' '}
+            <strong>Pick the day and time slot that’s best for you</strong>, and
+            tell us what you’d like to pair on.
+          </p>
+          <p>
+            You’ll receive a copy of your time slot request, and we’ll confirm
+            as we match you to a Double Agent Slots will be filled in order
+            received, and when slots fill up we’ll start a wait list.
+          </p>
+          <p>
+            If something comes up, please contact us so we can give your slot to
+            someone else.
+          </p>
+
+          <Script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></Script>
+          <iframe
+            src={PAIR_PROGRAMMING_FORM_EMBED}
+            width="100%"
+            height="1231"
+            className="airtable-embed airtable-dynamic-height mb-12"
+            style={{ background: '#ffffff' }}
+          ></iframe>
+        </div>
       </div>
     </Layout>
   );
