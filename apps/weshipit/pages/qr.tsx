31f5c2a02072ecb39bcfe1edd19673df8b50c566
David Leuliette <dleuliette@gmail.com>
2023-05-05 16:14:53 +0200

    feat: 🎸 add landing page for people who QR code business card (#97)
    * feat: 🎸 add landing page for people who QR code business card
    
    * feat: 🎸 add @tailwindcss/typography for prose component

---
+++ apps/weshipit/pages/qr.tsx
@@ -0,0 +1,50 @@
+import { Text } from '@weshipit/ui';
+import { Layout } from '../components/layout';
+import Script from 'next/script';
+
+const PAIR_PROGRAMMING_FORM_EMBED =
+  'https://airtable.com/embed/shrbgrlfblUkIFovF';
+
+export function Qr() {
+  return (
+    <Layout
+      seoTitle="FREE Pair Programming Session"
+      seoDescription={''}
+      withHeader
+      withFooter
+    >
+      <div className="mx-auto max-w-2xl p-3">
+        <div className="prose prose-lg">
+          <h1 className="mt-8">Office Hours</h1>
+          <p>
+            Join us during Code and Coffee breaks to pair with one of our
+            experienced software consultants on a challenge you’re puzzling
+            over!{' '}
+            <strong>Pick the day and time slot that’s best for you</strong>, and
+            tell us what you’d like to pair on.
+          </p>
+          <p>
+            You’ll receive a copy of your time slot request, and we’ll confirm
+            as we match you to a Double Agent Slots will be filled in order
+            received, and when slots fill up we’ll start a wait list.
+          </p>
+          <p>
+            If something comes up, please contact us so we can give your slot to
+            someone else.
+          </p>
+
+          <Script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></Script>
+          <iframe
+            src={PAIR_PROGRAMMING_FORM_EMBED}
+            width="100%"
+            height="1231"
+            className="airtable-embed airtable-dynamic-height mb-12"
+            style={{ background: '#ffffff' }}
+          ></iframe>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Qr;
--- libs/ui/src/lib/hero/hero.tsx
+++ libs/ui/src/lib/hero/hero.tsx
@@ -4,7 +4,7 @@ interface HeroProps {
 
 export function Hero({ children }: HeroProps) {
   return (
-    <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-center lg:py-72 lg:text-left">
+    <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-center lg:py-56 lg:text-left">
       {children}
     </div>
   );
