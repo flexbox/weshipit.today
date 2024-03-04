906bfeb91b6ea3c996073e6f0ffd225b1646c7db
David Leuliette <dleuliette@gmail.com>
2024-03-04 21:08:47 +0100

    feat: 🎸 add util library

---
+++ libs/util/src/lib/util.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface UtilProps {}
+
+export function Util(props: UtilProps) {
+  return (
+    <div>
+      <h1>Welcome to Util!</h1>
+    </div>
+  );
+}
+
+export default Util;
