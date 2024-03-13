9db791305e37cd2c271ee2b29a8f5638a5d5c6f4
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-03-13 21:46:55 +0100

    Fix Hero  (#166)
    * feat: 🎸 create component for Hero Text
    
    ✅ Closes: #153
    
    * fix: 🐛 add HeroText in all Hero
    
    * feat: 🎸 add stories for hero-text
    
    * fix: 🐛 add HeroText in Hero
    
    * fix: 🐛 remove useless files

--- apps/weshipit/pages/pricing.tsx
+++ apps/weshipit/pages/pricing.tsx
@@ -20,7 +20,7 @@ export default function PricingPage() {
       seoTitle="Subscription as a Service for React Native Developers"
       seoDescription="Software development is a service, not a product. We offer a subscription-based service for React Native developers. One flat fee. Zero billable hours. Pause or cancel whenever."
     >
-      <Hero>
+      <Hero title="">
         <Text as="h1" variant="h1" className="text-center">
           One flat fee.
           <br />
