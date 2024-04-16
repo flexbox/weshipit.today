09b05e74fb1779040ddc9769db31fab6f38d8e57
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-04-16 14:10:09 +0200

    feat: 🎸 add card and grid for french apps

--- apps/weshipit/pages/french-react-native-apps.tsx
+++ apps/weshipit/pages/french-react-native-apps.tsx
@@ -1,6 +1,5 @@
 import { Card, Hero, LinkButton, Prose } from '@weshipit/ui';
 
-import { linksApi } from './api/links';
 import { Layout } from '../components/layout';
 import client from './api/apollo-client';
 import { gql } from '@apollo/client';
@@ -33,6 +32,7 @@ export async function getStaticProps() {
     `,
   });
   const records: FrenchApp[] = data.getFrenchAppsRecords;
+  console.log('🚀 ~ getStaticProps ~ records:', records);
 
   return {
     props: {
@@ -57,49 +57,61 @@ export default function FrenchReactNativePage({
       seoDescription="Discover how French companies like BlaBlaCar, Doctolib, Ledger, and Shine are harnessing the power of React Native to create robust mobile applications."
     >
       <Hero title={heroTitle}></Hero>
-
-      <Prose>
+      <Prose size={'xl'} className="my-8">
         <p>
           We are building a list of French iOS and Android apps apps that are
           using React Native in {currentYear}. If you’re working in a French
           company that uses React Native, <a href={''}>add your app</a> to the
           list.
         </p>
-        <ul>
-          {records.map((record) => (
-            <>
-              <h3>{record.fields.name}</h3>
-              <p>{record.fields.category}</p>
-
-              <div className="flex gap-4">
-                <a
-                  href={record.fields.website_url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Website
-                </a>
-
-                <a
-                  href={record.fields.ios_url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  iOS
-                </a>
-
-                <a
-                  href={record.fields.android_url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Android
-                </a>
-              </div>
-            </>
-          ))}
-        </ul>
       </Prose>
+      <ul className="my-8">
+        <div className="grid grid-cols-3 gap-4">
+          {records.map((record) => (
+            <Card key={record.fields.name} size={'md'}>
+              <Prose>
+                <div className="flex gap-4">
+                  {/* <img
+                src={record.fields.logo[0].url}
+                alt={record.fields.name}
+                className="size-24 rounded-full object-cover"
+              /> */}
+                  ici image
+                  <div className="m-auto w-2/3">
+                    <h3 className="mt-0">{record.fields.name}</h3>
+                    <p>{record.fields.category}</p>
+                    <div className="flex gap-4">
+                      <a
+                        href={record.fields.website_url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Website
+                      </a>
+
+                      <a
+                        href={record.fields.ios_url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        iOS
+                      </a>
+
+                      <a
+                        href={record.fields.android_url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Android
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </Prose>
+            </Card>
+          ))}
+        </div>
+      </ul>
     </Layout>
   );
 }
