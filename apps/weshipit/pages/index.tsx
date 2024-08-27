af986ce27d6c6dc225381775abae0e81d2cbccdd
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-08-27 16:44:01 +0200

    feat: 🎸 add bento on homepage
    ✅ Closes: #200

--- apps/weshipit/pages/index.tsx
+++ apps/weshipit/pages/index.tsx
@@ -411,6 +411,120 @@ export default function IndexPage({
             </Button>
           </div>
           <div className="mb-16 py-0 pb-12 lg:py-24">
+            <>
+              <div className="m-auto grid w-3/4 grid-cols-2 gap-4 p-4">
+                <div className="flex h-full flex-col gap-4">
+                  <Card className="h-1/2 overflow-hidden p-4">
+                    <Prose size={'xl'} className="mb-6">
+                      <Text>
+                        A new mobile application{' '}
+                        <strong>quickly on Apple and Google store</strong>.
+                      </Text>
+                    </Prose>
+                    <div className="flex w-full justify-center gap-8">
+                      <Image
+                        className="rounded-lg"
+                        src="/images/gumroad.jpg"
+                        alt="iOS and Android logos"
+                        width={375}
+                        height={150}
+                      />
+                    </div>
+                  </Card>
+                  <Card className="h-1/2 overflow-hidden p-4">
+                    <Prose size={'xl'} className="mb-6">
+                      <Text>
+                        To <strong>stop struggling</strong> with React Native
+                        updates.
+                      </Text>
+                    </Prose>
+                    <div className="flex w-full justify-center gap-8">
+                      <Image
+                        className="rounded-lg"
+                        src="/images/update.png"
+                        alt="iOS and Android logos"
+                        width={375}
+                        height={250}
+                      />
+                    </div>
+                  </Card>
+                </div>
+                <div className="flex h-full flex-col gap-4">
+                  <Card className="h-1/3 p-4">
+                    <Prose size={'xl'} className="mb-6">
+                      <Text>
+                        To <strong>regain agility</strong> to evolve my
+                        application quickly.
+                      </Text>
+                    </Prose>
+                    <div className="flex justify-end gap-8">
+                      <Text variant="h2" className="pr-4 pt-4">
+                        🚀
+                      </Text>
+                    </div>
+                  </Card>
+                  <div className="flex h-1/3 w-full gap-4">
+                    <Card
+                      variant={'gradient-purple-dark'}
+                      className="w-1/2 p-4"
+                    >
+                      <Prose size={'xl'} className="mb-6">
+                        <Text>
+                          Talk to <strong>experts</strong>
+                        </Text>
+                      </Prose>
+                      <div className="relative mx-auto flex h-[164px] w-full flex-row items-center justify-center sm:h-[124px]">
+                        <Gravatar
+                          className="rounded-full border-4 border-white bg-slate-300 dark:bg-slate-700"
+                          size={80}
+                          email="ducrocq.matthys@gmail.com"
+                        />
+                        <Gravatar
+                          className="-ml-4 mr-4 rounded-full border-4 border-white bg-slate-300 dark:bg-slate-700"
+                          size={80}
+                          email="dleuliette@gmail.com"
+                        />
+                      </div>
+                    </Card>
+                    <Card className="w-1/2 p-4">
+                      <div className="mb-6 flex flex-col gap-2">
+                        <Text>
+                          <strong>Slack !</strong>
+                        </Text>
+                        <Text variant="c1">
+                          No emails here! Responsive and more human exchanges
+                        </Text>
+                      </div>
+                      <div className="flex justify-end gap-8">
+                        <Image
+                          src="/images/slack.png"
+                          alt="slack logo"
+                          width={50}
+                          height={50}
+                        />
+                      </div>
+                    </Card>
+                  </div>
+                  <Card className="h-1/3 p-4">
+                    <Prose size={'xl'} className="mb-6">
+                      <Text>
+                        To <strong>quickly migrate</strong> an application to
+                        React Native.
+                      </Text>
+                    </Prose>
+                    <div className="flex gap-8">
+                      <Image
+                        className="rounded-xl p-2 dark:bg-white"
+                        src="/images/expo-logo.png"
+                        alt="iOS and Android logos"
+                        width={175}
+                        height={75}
+                      />
+                    </div>
+                  </Card>
+                </div>
+              </div>
+            </>
             <div className="m-auto max-w-2xl">
               <Prose className="mb-12" size="xl">
                 <h2>Mobile app for iOS, Android and more</h2>
