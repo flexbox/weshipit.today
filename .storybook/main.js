66ff295d418d36626b42ddb063a974261c70b35e
David Leuliette <dleuliette@gmail.com>
2024-10-11 11:39:13 +0200

    feat: 🎸 add a11y for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials', '@chromatic-com/storybook'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y',
+  ],
 
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
