125450cc4b3d6aa445b5da092d9faea83b1e0a0e
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2025-02-28 14:20:49 +0100

    fix: 🐛 images link (#217)
    * fix: 🐛 images link
    
    * fix: 🐛 avatar /about
    
    ✅ Closes: #214

--- libs/ui/src/lib/gravatar/avatar-avengers.tsx
+++ libs/ui/src/lib/gravatar/avatar-avengers.tsx
@@ -16,27 +16,23 @@ export function AvatarAvenger({
 }: AvatarAvengerProps) {
   const [isShown, setIsShown] = useState(false);
   return (
-    <div className=" mb-8 flex h-48 w-full flex-row justify-around">
-      <div className="text-center">
-        <div
-          className="text-center"
-          onMouseEnter={() => setIsShown(true)}
-          onMouseLeave={() => setIsShown(false)}
-        >
-          {!isShown ? (
-            <div className="mb-4 size-36 rounded-full bg-gradient-to-b from-gray-200 to-gray-400 p-0.5">
-              <Avatar email={email} size={128} name={username} />
-            </div>
-          ) : (
-            <div className="mb-4 size-36 rounded-full bg-gradient-to-b from-gray-200 to-gray-400 p-0.5">
-              <Avatar email={emailhover} size={128} name={username} />
-            </div>
-          )}
+    <div className="flex flex-col items-center">
+      <div
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
+        <div className="size-32 rounded-full bg-gradient-to-b from-gray-200 to-gray-400 p-0.5">
+          <Avatar
+            className="rounded-full"
+            email={isShown ? emailhover : email}
+            size={128}
+            name={username}
+          />
         </div>
-        <a className="text-gray-600 hover:text-gray-400" href={githubUrl}>
-          {username}
-        </a>
       </div>
+      <a className="mt-2 text-gray-600 hover:text-gray-400" href={githubUrl}>
+        {username}
+      </a>
     </div>
   );
 }
