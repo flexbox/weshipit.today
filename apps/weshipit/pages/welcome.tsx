0417fd0ba93be3ce8fe4a07912d12726232f5312
David Leuliette <dleuliette@gmail.com>
2025-08-16 21:27:41 +0200

    feat: 🎸 add welcome page for kid go

---
+++ apps/weshipit/pages/welcome.tsx
@@ -0,0 +1,21 @@
+import { Layout } from '../components/layout';
+
+import { Prose } from '@weshipit/ui';
+
+export function Welcome() {
+  return (
+    <Layout seoTitle="Bienvenue sur Kid GO" seoDescription="" withHeader>
+      <div className="mx-auto max-w-2xl p-3">
+        <Prose size="lg">
+          <h1 className="mt-8">Compte Kid GO validé 🙌</h1>
+          <p>
+            Kid GO est votre compagnon mobile de référence pour les parents à la
+            recherche d'expériences amusantes, locales et adaptées aux familles.
+          </p>
+        </Prose>
+      </div>
+    </Layout>
+  );
+}
+
+export default Welcome;
